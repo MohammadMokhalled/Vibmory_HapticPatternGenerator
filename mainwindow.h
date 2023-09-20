4d7c3a712cb9ec05e54af3f85c1c53b8ba1d678c
Mohammad Mokhalled <m.mokhalled.95@gmail.com>
2023-09-20 22:21:41 +0300

    add comment to mainwindow

--- mainwindow.h
+++ mainwindow.h
@@ -4,24 +4,45 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+    class MainWindow;
+}
 QT_END_NAMESPACE
 
+/**
+ * @class MainWindow
+ * 
+ * The MainWindow class represents the main window of the application.
+ */
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * Constructs a MainWindow object.
+     * 
+     * @param parent The parent QWidget object.
+     */
     MainWindow(QWidget *parent = nullptr);
+
     ~MainWindow();
 
 private slots:
+    /**
+     * This function is called when the new project button is clicked.
+     * It handles the logic for creating a new project.
+     */
     void on_newProjectButton_clicked();
 
+    /**
+     * This function is called when the upload button is clicked.
+     */
     void on_uploadButton_clicked();
 
 private:
-    Ui::MainWindow* ui;
-    QMainWindow* mSecondWindow;
+    Ui::MainWindow *ui;
+    QMainWindow *mSecondWindow;
 };
 #endif // MAINWINDOW_H
