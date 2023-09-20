3de208ab31487e1fb7940d8675164ff1352475af
Mohammad Mokhalled <m.mokhalled.95@gmail.com>
2023-09-20 22:15:49 +0300

    add comment to createprojectwindow

--- createprojectwindow.h
+++ createprojectwindow.h
@@ -4,25 +4,45 @@
 #include <QMainWindow>
 #include "projectsettingwindow.h"
 
-namespace Ui {
-class CreateProjectWindow;
+namespace Ui
+{
+    class CreateProjectWindow;
 }
 
+
+/**
+ * @class CreateProjectWindow
+ * 
+ * @brief A class representing the Create Project Window.
+ *
+ * This class inherits from QMainWindow and provides functionality for creating a new project.
+ */
 class CreateProjectWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor for the CreateProjectWindow class.
+     *
+     * @param parent The parent widget.
+     */
     explicit CreateProjectWindow(QWidget *parent = nullptr);
+
     ~CreateProjectWindow();
 
-
 private slots:
+    /**
+     * @brief This function is called when the okButton is clicked.
+     *
+     * @details This function handles the click event of the okButton.
+     * It performs the necessary actions when the button is clicked.
+     */
     void on_okButton_clicked();
 
 private:
     Ui::CreateProjectWindow *ui;
-    QMainWindow* mSecondWindow;
+    QMainWindow *mSecondWindow;
 };
 
 #endif // CREATEPROJECT_H
