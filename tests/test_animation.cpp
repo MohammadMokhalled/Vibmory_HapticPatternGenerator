820c58963af2778b2c255a18ee0080e7c6d02dc9
Mohammad Mokhalled <m.mokhalled.95@gmail.com>
2023-10-03 20:44:24 +0300

    add resource files for animation test

--- tests/test_animation.cpp
+++ tests/test_animation.cpp
@@ -1,56 +1,59 @@
-#include <gtest/gtest.h>
 #include "animation.h"
+#include <QFile>
+#include <gtest/gtest.h>
 
-class AnimationTest : public ::testing::Test {
-protected:
-    void SetUp() override {
-        // Setup if needed
+class AnimationTest : public ::testing::Test
+{
+        protected:
+    void SetUp() override
+    {
     }
 
-    void TearDown() override {
-        // Cleanup if needed
+    void TearDown() override
+    {
     }
 };
 
-// 1. Test Default Constructor
-TEST_F(AnimationTest, DefaultConstructor) {
+TEST_F(AnimationTest, DefaultConstructor)
+{
     QSize size(200, 200);
     Animation anim(size);
     EXPECT_EQ(anim.getSize(), size);
     EXPECT_EQ(anim.getCurrentFrameIndex(), 0);
 }
 
-// // 2. Test File Constructor with Valid File
-// TEST_F(AnimationTest, FileConstructor_ValidFile) {
-//     QString filePath = "path_to_valid_file";
-//     Animation anim(filePath);
-//     EXPECT_FALSE(anim.getError());
-// }
+TEST_F(AnimationTest, FileConstructor_ValidFile)
+{
 
-// // 3. Test File Constructor with Invalid File
-// TEST_F(AnimationTest, FileConstructor_InvalidFile) {
-//     QString filePath = "path_to_invalid_file";
-//     ASSERT_THROW(Animation anim(filePath), std::runtime_error);
-// }
+    QString filePath = "resources/valid.csv";
+    Animation anim(filePath);
+    EXPECT_FALSE(anim.getError());
+}
 
-// 4. Test Add Frame
-TEST_F(AnimationTest, AddFrame) {
+TEST_F(AnimationTest, FileConstructor_InvalidFile)
+{
+    QString filePath = "resources/invalid.csv";
+    ASSERT_THROW(Animation anim(filePath), std::runtime_error);
+}
+
+TEST_F(AnimationTest, AddFrame)
+{
     QSize size(200, 200);
     Animation anim(size);
     anim.addFrame();
     EXPECT_EQ(anim.getLen(), 2);
 }
 
-// 5. Test Duplicate Current Frame
-TEST_F(AnimationTest, DuplicateCurrentFrame) {
+TEST_F(AnimationTest, DuplicateCurrentFrame)
+{
     QSize size(200, 200);
     Animation anim(size);
     anim.duplicateCurrentFrame();
     EXPECT_EQ(anim.getLen(), 2);
 }
 
-// 6. Test Frame Selection
-TEST_F(AnimationTest, SelectFrame) {
+TEST_F(AnimationTest, SelectFrame)
+{
     QSize size(100, 100);
     Animation anim(size);
     anim.addFrame();
@@ -58,17 +61,8 @@ TEST_F(AnimationTest, SelectFrame) {
     EXPECT_EQ(anim.getCurrentFrameIndex(), 1);
 }
 
-// 7. Test Position Setting
-TEST_F(AnimationTest, SetPosition) {
-    QSize size(100, 100);
-    Animation anim(size);
-    QPoint point(50, 50);
-    anim.setPos(point);
-    // You might need some method to verify the set position.
-}
-
-// 8. Test Next Frame Logic
-TEST_F(AnimationTest, NextFrame) {
+TEST_F(AnimationTest, NextFrame)
+{
     QSize size(100, 100);
     Animation anim(size);
     anim.addFrame();
@@ -76,16 +70,16 @@ TEST_F(AnimationTest, NextFrame) {
     EXPECT_EQ(anim.getCurrentFrameIndex(), 1);
 }
 
-// 9. Test Next Frame Looping Logic
-TEST_F(AnimationTest, NextFrameLooping) {
+TEST_F(AnimationTest, NextFrameLooping)
+{
     QSize size(100, 100);
     Animation anim(size);
     anim.nextFrame();
     EXPECT_EQ(anim.getCurrentFrameIndex(), 0);
 }
 
-// 10. Test Removing Current Frame
-TEST_F(AnimationTest, RemoveCurrentFrame) {
+TEST_F(AnimationTest, RemoveCurrentFrame)
+{
     QSize size(100, 100);
     Animation anim(size);
     anim.addFrame();
@@ -93,43 +87,49 @@ TEST_F(AnimationTest, RemoveCurrentFrame) {
     EXPECT_EQ(anim.getLen(), 1);
 }
 
-// 11. Test Getting Frame String
-TEST_F(AnimationTest, GetFrameString) {
+TEST_F(AnimationTest, GetFrameString)
+{
     QSize size(100, 100);
     Animation anim(size);
     QString frameStr = anim.getFrameString();
     EXPECT_FALSE(frameStr.isEmpty());
 }
 
-// 12. Test Writing To File
-// TEST_F(AnimationTest, WriteToFile) {
-//     QSize size(100, 100);
-//     Animation anim(size);
-//     QString outputPath = "path_to_output_file";
-//     EXPECT_TRUE(anim.writeInFile(outputPath));
-// }
+TEST_F(AnimationTest, WriteToFile)
+{
+    QSize size(100, 100);
+    Animation anim(size);
+    QString outputPath = "test.csv";
+    EXPECT_TRUE(anim.writeInFile(outputPath));
+    EXPECT_FALSE(anim.getError());
+    EXPECT_TRUE(QFile::exists(outputPath));
 
-// 13. Test Error Flag On Default Constructor
-TEST_F(AnimationTest, NoErrorOnDefaultConstructor) {
+    Animation anim2(outputPath);
+    EXPECT_FALSE(anim2.getError());
+    EXPECT_EQ(anim2.getLen(), anim.getLen());
+
+    QFile::remove(outputPath);
+}
+
+TEST_F(AnimationTest, NoErrorOnDefaultConstructor)
+{
     QSize size(100, 100);
     Animation anim(size);
     EXPECT_FALSE(anim.getError());
 }
 
-// 14. Test Getting Amplitude
-TEST_F(AnimationTest, GetAmplitude) {
+TEST_F(AnimationTest, GetAmplitude)
+{
     QSize size(100, 100);
     Animation anim(size);
     QPoint point(50, 50);
-    // Assuming default amplitude value is 0 for a new frame
     EXPECT_EQ(anim.getAmplitude(point), 0);
 }
 
-// 15. Test Getting Frequency
-TEST_F(AnimationTest, GetFrequency) {
+TEST_F(AnimationTest, GetFrequency)
+{
     QSize size(100, 100);
     Animation anim(size);
     QPoint point(50, 50);
-    // Assuming default frequency value is 0 for a new frame
     EXPECT_EQ(anim.getFrequency(point), 0);
 }
--- tests/test_frame.cpp
+++ tests/test_frame.cpp
@@ -1,21 +1,25 @@
 #include "frame.h"
 #include <gtest/gtest.h>
 
-class FrameTest : public ::testing::Test {
-protected:
-    void SetUp() override {
+class FrameTest : public ::testing::Test
+{
+        protected:
+    void SetUp() override
+    {
         // Set up logic, if needed.
     }
 };
 
-TEST_F(FrameTest, ConstructorWithQSize) {
+TEST_F(FrameTest, ConstructorWithQSize)
+{
     QSize size(10, 10);
     Frame frame(size);
     EXPECT_EQ(frame.toString().count('\n'), size.height());
     EXPECT_EQ(frame.toString().count(','), size.width() * size.height() * 2);
 }
 
-TEST_F(FrameTest, ConstructorWithCSVAndSize) {
+TEST_F(FrameTest, ConstructorWithCSVAndSize)
+{
     QSize size(2, 2);
     QString csv = "0, 0, 0, 0,\n0, 0, 0, 0,\n";
     Frame frame(csv, size);
@@ -23,7 +27,8 @@ TEST_F(FrameTest, ConstructorWithCSVAndSize) {
     EXPECT_EQ(frame.toString().count(','), size.width() * size.height() * 2);
 }
 
-TEST_F(FrameTest, SetAndGetAmplitude) {
+TEST_F(FrameTest, SetAndGetAmplitude)
+{
     QSize size(10, 10);
     Frame frame(size);
     QPoint pos(5, 5);
@@ -31,7 +36,8 @@ TEST_F(FrameTest, SetAndGetAmplitude) {
     EXPECT_EQ(frame.getAmplitude(pos), 100);
 }
 
-TEST_F(FrameTest, SetAndGetFrequency) {
+TEST_F(FrameTest, SetAndGetFrequency)
+{
     QSize size(10, 10);
     Frame frame(size);
     QPoint pos(5, 5);
@@ -39,7 +45,8 @@ TEST_F(FrameTest, SetAndGetFrequency) {
     EXPECT_EQ(frame.getFrequency(pos), 50);
 }
 
-TEST_F(FrameTest, SetAndGetColor) {
+TEST_F(FrameTest, SetAndGetColor)
+{
     QSize size(10, 10);
     Frame frame(size);
     QPoint pos(5, 5);
@@ -50,20 +57,18 @@ TEST_F(FrameTest, SetAndGetColor) {
     EXPECT_EQ(color.saturation(), 127);
 }
 
-TEST_F(FrameTest, InvalidIndexCheck) {
+TEST_F(FrameTest, InvalidIndexCheck)
+{
     QSize size(10, 10);
     Frame frame(size);
     EXPECT_EQ(frame.getAmplitude(QPoint(11, 5)), -1);
     EXPECT_EQ(frame.getFrequency(QPoint(5, 11)), -1);
 }
 
-TEST_F(FrameTest, ErrorCheck) {
-   QSize size(2, 2);
-   QString invalidCsv = "0, 0, 0,\n0, 0, 0, 0,\n";
-   // Expecting that constructor of Frame will throw an exception of any type
-    EXPECT_THROW(
-      [&]() { Frame frame(invalidCsv, size); }(),
-      std::exception
-   );
+TEST_F(FrameTest, ErrorCheck)
+{
+    QSize size(2, 2);
+    QString invalidCsv = "0, 0, 0,\n0, 0, 0, 0,\n";
+    // Expecting that constructor of Frame will throw an exception of any type
+    EXPECT_THROW([&]() { Frame frame(invalidCsv, size); }(), std::exception);
 }
-
